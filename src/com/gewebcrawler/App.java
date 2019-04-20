7787f1ce0e21362684111f56ec88b88f2f3f2d84
Balry <michaelpm88@hotmail.com>
2019-04-20 03:16:07 -0400

    Threads v2 + finalize realized how threads work...

--- src/com/gewebcrawler/App.java
+++ src/com/gewebcrawler/App.java
@@ -1,34 +1,47 @@
 package com.gewebcrawler;
 
+import com.gewebcrawler.json.GSONFileReader;
+import com.gewebcrawler.json.Internet;
+import com.gewebcrawler.json.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class App {
+    private static Internet internet;
+    private static final List<String> internetAddresses = new ArrayList<>();  //Comprehensive list of addresses in the Internet
 
     public static void main(String[] args){
         try{
 
-            Thread webCrawler = new WebCrawler();
+            try {
+                GSONFileReader gsonFileReader = new GSONFileReader("reqDoc\\internet_1.json");  //Read JSON File
+                internet = gsonFileReader.deserializeInternetJSON();                                  //Deserialize JSON file
+            }catch (Exception e) { e.getStackTrace(); }
 
-            while (Database.getInstance().getUnvisitedLinks().size() > 0) {
-                webCrawler.start();
-
-
-                /*System.out.println("Number of Threads: " + Thread.activeCount());
-                Thread[] listOfThreads = new Thread[Thread.activeCount()];
-                Thread.enumerate(listOfThreads);
-                for(Thread i:listOfThreads){
-                    System.out.println(i.getName());
-                }*/
-
-                try{
-                    Thread.sleep(1);
-                }catch (InterruptedException e){}
-
+            //Retrieve all addresses from the internet
+            for(Page page: internet.getPages()) {
+                internetAddresses.add(page.getAddress());
             }
+
+            Database.getInstance().getUnvisitedLinks().add(internetAddresses.get(internetAddresses.size()-1));      //Select starting page
+            Database.getInstance().getUnvisitedLinks().add(internetAddresses.get(0));                               //Select starting page
+
+            Thread webCrawler0 = new WebCrawler(internet, internetAddresses, 0);
+            Thread webCrawler1 = new WebCrawler(internet, internetAddresses, 1);
+
+            webCrawler0.start();
+            webCrawler1.start();
+
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){}
 
             //Console output
             System.out.println("Success:\n" + Database.getInstance().getSuccess().toString());
             System.out.println("\nSkipped:\n" + Database.getInstance().getSkipped().toString());
             System.out.println("\nError:\n" + Database.getInstance().getError().toString());
 
-        }catch (Exception e){ e.getStackTrace(); }
+        }catch (Exception e){ e.getStackTrace();  System.out.println(e); }
     }
 }
--- src/com/gewebcrawler/WebCrawler.java
+++ src/com/gewebcrawler/WebCrawler.java
@@ -11,42 +11,32 @@ import java.util.List;
 //TODO only spans one thread... maybe if json was larger it would span more?
 public class WebCrawler extends Thread {
     private Internet internet;
-    private static final List<String> internetAddresses = new ArrayList<>();  //Comprehensive list of addresses in the Internet
+    private static List<String> internetAddresses;  //Comprehensive list of addresses in the Internet
     private Database database = Database.getInstance();
+    private int index;
 
-    public WebCrawler() {
-
-        try {
-            GSONFileReader gsonFileReader = new GSONFileReader("reqDoc\\internet_1.json");  //Read JSON File
-            internet = gsonFileReader.deserializeInternetJSON();                                  //Deserialize JSON file
-        }catch (Exception e) { e.getStackTrace(); }
-
-        //Retrieve all addresses from the internet
-        for(Page page: internet.getPages()) {
-            internetAddresses.add(page.getAddress());
-        }
-
-        //database.getUnvisitedLinks().add(internetAddresses.get(internetAddresses.size()-1));      //Select starting page
-        database.getUnvisitedLinks().add(internetAddresses.get(0));                                 //Select starting page
-
+    public WebCrawler(Internet internet, List<String> internetAddresses, int index ) {
+        this.internet = internet;
+        this.internetAddresses = internetAddresses;
+        this.index = index;
     }
 
     @Override
     public void run() {
         //TODO optimize if statements
         //Visit new links in order
-        while (database.getUnvisitedLinks().size() > 0) {
+        while (database.getUnvisitedLinks().size() > 0 && database.getUnvisitedLinks().get(index) != null) {
             //Mark as error if link does not exist in list of Pages in Internet and not in error list
-            if (!internetAddresses.contains(database.getUnvisitedLinks().get(0)) && !database.getError().contains(database.getUnvisitedLinks().get(0))){
+            if (!internetAddresses.contains(database.getUnvisitedLinks().get(0)) && !database.getError().contains(database.getUnvisitedLinks().get(index))){
                 database.getError().add(database.getUnvisitedLinks().get(0));
             }
             //Mark as successful if not visited yet and not in error list
-            else if(!database.getSuccess().contains(database.getUnvisitedLinks().get(0)) && !database.getError().contains(database.getUnvisitedLinks().get(0))) {
+            else if(!database.getSuccess().contains(database.getUnvisitedLinks().get(0)) && !database.getError().contains(database.getUnvisitedLinks().get(index))) {
                 database.getSuccess().add(database.getUnvisitedLinks().get(0));
-                database.getUnvisitedLinks().addAll(Arrays.asList(internet.getPages()[internetAddresses.indexOf(database.getUnvisitedLinks().get(0))].getLinks()));
+                database.getUnvisitedLinks().addAll(Arrays.asList(internet.getPages()[internetAddresses.indexOf(database.getUnvisitedLinks().get(index))].getLinks()));
             }
             //Mark as skipped if not in skipped list and not in error list
-            else if(!database.getSkipped().contains(database.getUnvisitedLinks().get(0)) && !database.getError().contains(database.getUnvisitedLinks().get(0))) {
+            else if(!database.getSkipped().contains(database.getUnvisitedLinks().get(0)) && !database.getError().contains(database.getUnvisitedLinks().get(index))) {
                 database.getSkipped().add(database.getUnvisitedLinks().get(0));
             }
             database.getUnvisitedLinks().remove(0);                                       //Remove checked link
